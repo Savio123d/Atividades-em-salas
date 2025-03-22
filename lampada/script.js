b32aeaa028a7ab8fbbe03fffd8d370740031c421
Savio123d <savio13foxy@gmail.com>
2025-03-21 22:25:05 -0300

    Create script.js

---
+++ lampada/script.js
@@ -0,0 +1,9 @@
+function ligar(){
+    let lampada = document.getElementById("lampada");
+    lampada.src = "imagens/ligada.avif";
+  }
+
+  function desligar(){
+    let lampada = document.getElementById("lampada");
+    lampada.src = "imagens/desligada.avif";
+  }
