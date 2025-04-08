4d8a387bd513bcac82b9cce9d566e17e644ddb13
Savio123d <savio13foxy@gmail.com>
2025-04-07 21:14:19 -0300

    Create script.js

---
+++ list com array/js/script.js
@@ -0,0 +1,22 @@
+const botao = document.getElementById('add');
+const entrada = document.getElementById('entrada');
+const lista = document.getElementById('lista');
+
+const items = [];
+
+botao.addEventListener('click',
+    function () {
+        let ValorInput = entrada.value;
+        items.push(ValorInput);
+        atualizar();
+
+    }
+
+)
+function atualizar(){
+    lista.innerHTML = null;
+    let li = document.createElement("li");
+    li.textContent = "Deu certo";
+    lista.appendChild(li);
+
+}
